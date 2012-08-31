f5587f8ff954d492bd2b8760bb73447cc01f8284
Mimmi Thorneus <mimmi.thorneus@gmail.com>
2012-08-31 21:33:04 +0200

    Social media-knappar tillagda

--- header.php
+++ header.php
@@ -63,12 +63,16 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
+	<header id="masthead" class="site-header clearfix" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 		
+		<div id="social-media">
+			<a id="facebook" href=""><img alt="Facebook" src="<?php echo get_template_directory_uri(); ?>/images/fb.png"></a>
+			<a id="twitter" href=""><img alt="Twitter" src="<?php echo get_template_directory_uri(); ?>/images/twit.png"></a>
+		</div>
 	</header><!-- #masthead .site-header -->
 
 	<div id="wrapper">
