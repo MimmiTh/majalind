eb1061db3402706448082ce0d1f6cb9188f8e763
Mimmi Thorneus <mimmi.thorneus@gmail.com>
2012-09-01 21:44:46 +0200

    Styling av meny

--- header.php
+++ header.php
@@ -64,10 +64,10 @@
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header clearfix" role="banner">
-		<hgroup>
-			<h1 class="site-title"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
+		<div id="header-hgroup" class="clearfix">
+			<div class="site-title"><h1><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1></div>
+			<div class="site-description"><h2><?php bloginfo( 'description' ); ?></h2></div>
+		</div>
 		
 		<div id="social-media">
 			<a id="facebook" href=""><img alt="Facebook" src="<?php echo get_template_directory_uri(); ?>/images/fb.png"></a>
