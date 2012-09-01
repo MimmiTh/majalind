f7b56ad2798f341d8eaf13eb2220974b415aa684
Mimmi Thorneus <mimmi.thorneus@gmail.com>
2012-09-01 17:46:27 +0200

    Skuggor

--- publications.php
+++ publications.php
@@ -11,17 +11,29 @@ Template Name: Publikationer
 <?php $args = array( 'post_type' => 'publications', 'posts_per_page' => 100 );
 $loop = new WP_Query( $args );
 while ( $loop->have_posts() ) : $loop->the_post();
-	echo '<div class="publication clearfix">';
-	echo '<div class="publications-left">';
-	the_post_thumbnail();
-	echo '</div>';
-	echo '<div class="publications-right">';
-	echo '<h2>';
-	the_title();
-	echo '</h2>';
-	the_content();
-	echo '</div>';
-	echo '</div>';
+	if ( has_post_thumbnail() ) {
+		echo '<div class="publication clearfix">';
+		echo '<div class="publications-left">';
+		the_post_thumbnail();
+		echo '</div>';
+		echo '<div class="publications-right">';
+		echo '<h2>';
+		the_title();
+		echo '</h2>';
+		the_content();
+		echo '</div>';
+		echo '</div>';
+
+	} else {
+		echo '<div class="publication clearfix">';
+		echo '<div class="publication-content">';
+		echo '<h2>';
+		the_title();
+		echo '</h2>';
+		the_content();
+		echo '</div>';
+		echo '</div>';
+	}
 endwhile; ?>
 
 </div>
