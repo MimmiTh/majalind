65dfe84873e5cd09eb94f5f3daa007861e31972f
Mimmi Thorneus <mimmi.thorneus@gmail.com>
2012-09-06 16:06:35 +0200

    Footer utan widgets

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 				<?php do_action( 'maja_lind_credits' ); ?>
 				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'maja_lind' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'maja_lind' ), 'WordPress' ); ?></a>
 				<span class="sep"> | </span>
-				<?php printf( __( 'Theme: %1$s by %2$s.', 'maja_lind' ), 'maja_lind', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+				<?php printf( __( '<p>Tema skapat av: <a href="http://mimmithorneus.se">Mimmi Thorneus</a></p><p>Facebook- och Twitter-ikon från: <a href="http://sharevectors.com">Share Vectors</a></p>' ); ?>
 			</div><!-- .site-info -->
 		<?php endif; // end sidebar widget area ?>
 	</footer><!-- #colophon .site-footer -->
